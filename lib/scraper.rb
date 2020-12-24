0eb4662d5644d0b6f09ac329ba48b10fbcc7533b
Romano Livi <romano_livi@outlook.com>
2020-12-24 17:34:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
@@ -16,7 +16,11 @@ class Scraper
     end
   end
   
+  def get_page 
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
 end
 
-
+Scraper.new.get_page
 
